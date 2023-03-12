04fb59d2fba657e0bc66236d887c0f37072554eb
ateeq-ur-rahman <93069335+ateeq-ur-rahman@users.noreply.github.com>
2023-03-12 17:14:53 +0530

    Add files via upload

---
+++ todo-cli/todo.js
@@ -0,0 +1,42 @@
+const todoList = () => {
+  all = []
+  const add = (todoItem) => {
+    all.push(todoItem)
+  }
+  const markAsComplete = (index) => {
+    all[index].completed = true
+  }
+
+  const overdue = () => {
+    // Write the date check condition here and return the array of overdue items accordingly.
+    // FILL YOUR CODE HERE
+    return all.filter((todolist) =>
+      todolist.dueDate == yesterday
+    )
+  }
+
+  const dueToday = () => {
+    // Write the date check condition here and return the array of todo items that are due today accordingly.
+    // FILL YOUR CODE HERE
+    return all.filter((todolist) =>
+      todolist.dueDate == today
+    )
+  }
+
+  const dueLater = () => {
+    // Write the date check condition here and return the array of todo items that are due later accordingly.
+    // FILL YOUR CODE HERE
+    return all.filter((todolist) =>
+      todolist.dueDate == tomorrow
+    )
+  }
+
+  function toDisplayableList(list) {
+    // Format the To-Do list here, and return the output string as per the format given above.
+    // FILL YOUR CODE HERE
+    return list.map(todolist => `${todolist.completed ? `[x]` : `[ ]`} ${todolist.title} ${todolist.dueDate == today ?" ": todolist.dueDate}`).join('\n')
+  }
+  return { all, add, markAsComplete, overdue, dueToday, dueLater, toDisplayableList };
+}
+
+module.exports= todoList;
