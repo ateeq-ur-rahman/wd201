d7827f2a9738daf5951aecc1008f66cf3aea44b1
ateeq-ur-rahman <93069335+ateeq-ur-rahman@users.noreply.github.com>
2023-03-21 21:24:21 +0530

    Update todo.js

--- todo-cli/models/todo.js
+++ todo-cli/models/todo.js
@@ -1,99 +1,96 @@
+// models/todo.js
 'use strict';
-const {
-  Model,Op
-} = require('sequelize');
+const { Model } = require('sequelize');
+
 module.exports = (sequelize, DataTypes) => {
-  class Todo extends Model {
-    /**
-     * Helper method for defining associations.
-     * This method is not a part of Sequelize lifecycle.
-     * The `models/index` file will call this method automatically.
-     */
-    static async addTask(params){
-      return await Todo.create(params)
-    }
+class Todo extends Model {
+static async addTask(params) {
+return await Todo.create(params);
+}
+static async showList() {
+  console.log("My Todo-list\n");
 
-    displayableString(){
-      return `${this.id}. ${this.completed?'[x]':'[ ]'} ${this.title} ${this.dueDate === new Date().toLocaleDateString("en-CA")?"":this.dueDate}`.trim();
-    }
+  console.log("Overdue");
+  const overdue = await Todo.overdue();
+  overdue.forEach((todo) => console.log(todo.displayableString()));
+  console.log("\n");
 
-    static async showList(){
-      console.log("My Todo list \n");
+  console.log("Due Today");
+  const dueToday = await Todo.dueToday();
+  dueToday.forEach((todo) => console.log(todo.displayableString()));
+  console.log("\n");
 
-      console.log("Overdue");
-      console.log(
-        (await Todo.overdue())
-          .map((todo) => {
-            return todo.displayableString();
-          })
-          .join("\n")
-      );
-      console.log("\n");
+  console.log("Due Later");
+  const dueLater = await Todo.dueLater();
+  dueLater.forEach((todo) => console.log(todo.displayableString()));
+}
 
-      console.log("Due Today");
-      console.log(
-        (await Todo.dueToday())
-          .map((todo) => {
-            return todo.displayableString();
-          })
-          .join("\n")
-      );
-      console.log("\n");
-
-      console.log("Due Later");
-      console.log(
-        (await Todo.dueLater())
-          .map((todo) => {
-            return todo.displayableString();
-          })
-          .join("\n")
-      );
-    }
-
-    static async overdue() {
-      
-      return await Todo.findAll({
-        where: {
-          dueDate: { [Op.lt]: new Date().toLocaleDateString("en-CA") },
-        }
-  })}
-
-  static async dueToday() {
-      return await Todo.findAll({
-        where: {
-          dueDate: { [Op.eq]: new Date().toLocaleDateString("en-CA") },
-        },
-      });
-  }
-
-  static async dueLater() {
-    
-      return await Todo.findAll({
-        where: {
-          dueDate: { [Op.gt]: new Date().toLocaleDateString("en-CA") },
-        },
-      });
-  }
-
-  static async markAsComplete(id) {
-    await Todo.update(
-      { completed: true },
-      {
-        where: {
-          id: id,
-        },
-      }
-    );
-  }
-    
-  }
-  Todo.init({
-    title: DataTypes.STRING,
-    dueDate: DataTypes.DATEONLY,
-    completed: DataTypes.BOOLEAN
-  }, {
-    sequelize,
-    modelName: "Todo",
+static async overdue() {
+  return await Todo.findAll({
+    where: {
+      dueDate: {
+        [Op.lt]: new Date(),
+      },
+      completed: false,
+    },
+    order: [['dueDate', 'ASC']],
   });
-  return Todo;
+}
+
+static async dueToday() {
+  const today = new Date();
+  const tomorrow = new Date(today);
+  tomorrow.setDate(tomorrow.getDate() + 1);
+
+  return await Todo.findAll({
+    where: {
+      dueDate: {
+        [Op.gte]: today,
+        [Op.lt]: tomorrow,
+      },
+      completed: false,
+    },
+    order: [['dueDate', 'ASC']],
+  });
+}
+
+static async dueLater() {
+  const today = new Date();
+  const tomorrow = new Date(today);
+  tomorrow.setDate(tomorrow.getDate() + 1);
+
+  return await Todo.findAll({
+    where: {
+      dueDate: {
+        [Op.gte]: tomorrow,
+      },
+      completed: false,
+    },
+    order: [['dueDate', 'ASC']],
+  });
+}
+
+static async markAsComplete(id) {
+  const todo = await Todo.findByPk(id);
+  todo.completed = true;
+  await todo.save();
+}
+
+displayableString() {
+  let checkbox = this.completed ? "[x]" : "[ ]";
+  return `${this.id}. ${checkbox} ${this.title} ${this.dueDate.toDateString()}`;
+}
+}
+Todo.init(
+{
+title: DataTypes.STRING,
+dueDate: DataTypes.DATEONLY,
+completed: DataTypes.BOOLEAN,
+},
+{
+sequelize,
+modelName: 'Todo',
+}
+);
+return Todo;
 };
